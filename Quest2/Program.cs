c61184de4bfd6fb386e77fbe10d13645c7a8074b
Roman_Egorov <egorovrp1991@mail.ru>
2023-08-08 10:27:18 +0300

    End 2 Quest

--- Quest2/Program.cs
+++ Quest2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int GetNum(string message)
+{
+    Console.Write(message);
+	int num = int.Parse(Console.ReadLine()!);
+	return num;
+}
+
+void SumNatureNum(int m, int n)
+{
+    int sum = 0;
+    int i = m;
+    while (i != n+1)
+    {
+        sum = sum + i;
+        i++;
+    }
+    Console.Write($"Сумма натуральных чисел от {m} до {n} = {sum}");
+}
+
+int M = GetNum("Введите число M:");
+int N = GetNum("Введите число N:");
+SumNatureNum(M,N);
