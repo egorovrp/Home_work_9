fdad02acf61b91f9c66191bd5f183b93da948e58
Roman_Egorov <egorovrp1991@mail.ru>
2023-08-08 09:45:55 +0300

    End 1 Quest

--- Quest1/Program.cs
+++ Quest1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void Rekursion (int number) //на вход принимается число от пользователя N
+{   
+    if (number != 1)
+    {
+        Console.Write($"{number},");
+        Rekursion(number - 1);
+    }
+    else
+    {
+        if (number == 1)
+        {
+            Console.Write($"{number}");
+        }
+    
+    }
+}
+    
+Rekursion(8);
+
+
